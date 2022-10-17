34a4ea994626cd1e9e1b281e8e9b76246dcad73e
James (DPC) <james.ellis@dpc.nsw.gov.au>
2022-10-17 17:21:18 +1100

    Update tests to use fixtures

--- tests/DatawrapperTest.php
+++ tests/DatawrapperTest.php
@@ -1,50 +1,32 @@
 <?php
 
-namespace  NSWDPC\Elemental\Tests\Datawrapper;
+namespace NSWDPC\Elemental\Tests\Datawrapper;
 
-use gorriecoe\Link\Models\Link;
-use gorriecoe\LinkField\LinkField;
-use NSWDPC\Datawrapper\WebHookController;
 use NSWDPC\Elemental\Models\Datawrapper\ElementDatawrapper;
-use SilverStripe\Core\Config\Config;
-use SilverStripe\Control\Director;
 use SilverStripe\Dev\SapphireTest;
-use SilverStripe\Dev\FunctionalTest;
-use Silverstripe\Assets\Dev\TestAssetStore;
-use SilverStripe\Assets\File;
-use SilverStripe\Assets\Folder;
-use SilverStripe\Assets\Image;
-use SilverStripe\View\Requirements;
 
 /**
  * Unit test to verify Datawrapper element handling
  * @author James
  */
-class DatawrapperTest extends FunctionalTest
+class DatawrapperTest extends SapphireTest
 {
 
+    /**
+     * @inheritdoc
+     */
     protected $usesDatabase = true;
 
-    public function setUp() {
-        parent::setUp();
-        Config::inst()->set( WebHookController::class, 'webhooks_random_code', 'randomecodeforurl' );
-        Config::inst()->set( WebHookController::class, 'webhooks_enabled', true );
-    }
+    /**
+     * @inheritdoc
+     */
+    protected static $fixture_file = "./DataWrapperTest.yml";
 
-    public function tearDown()
-    {
-        parent::tearDown();
-    }
-
+    /**
+     * Return test record for test
+     */
     private function createTestRecord() {
-        return [
-            'Title' => 'Title',
-            'ShowTitle' => 1,
-            'IsLazy' => 1,
-            'IsFullWidth' => 1,
-            'IsResponsive' => '16x9',
-            'AlternateContent' => 'AlternateContent',
-        ];
+        return $this->objFromFixture(ElementDatawrapper::class, 'standardrecord');
     }
 
     /**
@@ -55,212 +37,75 @@ class DatawrapperTest extends FunctionalTest
             'DatawrapperId' => 'abcd4',
             'DatawrapperVersion' => 8
         ];
-        $iframe = ElementDatawrapper::create($record);
-        $iframe->InputURL = "https://"
-                                . $iframe->config()->get('default_host')
+        $record = ElementDatawrapper::create($record);
+        $record->InputURL = "https://"
+                                . ElementDatawrapper::config()->get('default_host')
                                 . "/"
-                                . $record['DatawrapperId']
+                                . $record->DatawrapperId
                                 . "/"
-                                . $record['DatawrapperVersion']
+                                . $record->DatawrapperVersion
                                 . "/";
-        $iframe->write();
+        $id = $record->write();
 
-        $this->assertEquals($iframe->DatawrapperURL(), $iframe->InputURL);
-        $this->assertEquals($record['DatawrapperId'], $iframe->DatawrapperId);
-        $this->assertEquals($record['DatawrapperVersion'], $iframe->DatawrapperVersion);
+        $updatedRecord = ElementDatawrapper::get()->byId($id);
+
+        $this->assertEquals($record->InputURL, $updatedRecord->DatawrapperURL() );
+        $this->assertEquals($record->DatawrapperId, $updatedRecord->DatawrapperId);
+        $this->assertEquals($record->DatawrapperVersion, $updatedRecord->DatawrapperVersion);
     }
 
+    /**
+     * Test iframe save/update
+     */
     public function testIframe() {
 
         $width = 300;
         $height = 200;
 
         $record = $this->createTestRecord();
-        $record['Title'] = 'IFRAME_TEST';
-        $record['DatawrapperId'] = 'test1';
-        $record['DatawrapperVersion'] = 12;
+        $record->DatawrapperId = 'test1';
+        $record->DatawrapperVersion = 12;
 
-        $record['Width'] = $width;
-        $record['Height'] = $height;
+        $record->Width = $width;
+        $record->Height = $height;
 
-        $iframe = ElementDatawrapper::create($record);
-        $iframe->InputURL = "https://"
-                                . $iframe->config()->get('default_host')
+        $record->InputURL = "https://"
+                                . ElementDatawrapper::config()->get('default_host')
                                 . "/"
-                                . $record['DatawrapperId']
+                                . $record->DatawrapperId
                                 . "/"
-                                . $record['DatawrapperVersion']
+                                . $record->DatawrapperVersion
                                 . "/";
-        $iframe->write();
+        $id = $record->write();
 
-        $this->assertEquals($iframe->DatawrapperURL(), $iframe->InputURL);
+        $updatedRecord = ElementDatawrapper::get()->byId($id);
 
-        $this->assertTrue($iframe->exists(), "Element datawrapper does not exist");
+        $this->assertTrue($updatedRecord->exists(), "Element datawrapper does not exist");
 
-        $iframe_width = $iframe->getIframeWidth();
+        $this->assertEquals($record->InputURL, $updatedRecord->DatawrapperURL() );
+
+        $iframe_width = $record->getIframeWidth();
         $this->assertEquals("100%", $iframe_width, "Responsive iframe should be 100% width");
 
-        $iframe_height = $iframe->getIframeHeight();
+        $iframe_height = $record->getIframeHeight();
         $this->assertEquals("200", $iframe_height, "Iframe should be {$height} height");
 
-        $template = $iframe->forTemplate();
+        $template = $record->forTemplate();
 
         $strings = [
-            "id=\"{$iframe->DatawrapperIdAttribute()}",
+            "id=\"{$record->DatawrapperIdAttribute()}",
             "allow=\"fullscreen\"",
             "loading=\"lazy\"",
             "height=\"{$iframe_height}\"",
-            "<h2>{$record['Title']}</h2>",
-            "title=\"{$record['AlternateContent']}\"",
-            "src=\"" . htmlspecialchars($iframe->DatawrapperURL()) . "\""
+            "<h2>{$record->Title}</h2>",
+            "title=\"{$record->AlternateContent}\"",
+            "src=\"" . htmlspecialchars($record->DatawrapperURL()) . "\""
         ];
 
         foreach($strings as $string) {
             $this->assertTrue(strpos($template, $string) !== false, "{$string} should appear in the template");
         }
 
-    }
-
-    public function testWebHook() {
-
-        $width = 300;
-        $height = 200;
-
-        $record = $this->createTestRecord();
-        $record['Title'] = 'WEBHOOK_TEST';
-        $record['DatawrapperId'] = 'hook1';
-        $record['DatawrapperVersion'] = 12;
-        $record['Width'] = $width;
-        $record['Height'] = $height;
-        $record['AutoPublish'] = 1;
-
-
-        $iframe = ElementDatawrapper::create($record);
-        $iframe->InputURL = "https://"
-                                . $iframe->config()->get('default_host')
-                                . "/"
-                                . $record['DatawrapperId']
-                                . "/"
-                                . $record['DatawrapperVersion']
-                                . "/";
-        $iframe->write();
-
-
-        $url = WebHookController::getWebookURL();
-        $headers = [
-            'Content-Type' => "application/json"
-        ];
-        $session = null;
-        $data = [
-            'id' => $record['DatawrapperId'],
-            'publicVersion' => 3
-        ];
-        $cookies = null;
-        $body = json_encode($data, JSON_UNESCAPED_SLASHES|JSON_PRETTY_PRINT);
-
-        $request = $this->post($url, $data, $headers, $session, $body, $cookies);
-        $response = json_decode($request->getBody());
-
-        $this->assertEquals(
-            1,
-            $response->count // expect 1 element
-        );
-
-        $this->assertEquals(
-            200,
-            $request->getStatusCode(),
-            'Expected success, got: ' . $request->getStatusCode() . "/" . $request->getStatusDescription()
-        );
-
-
-        // turn off autopublish
-        $iframe->AutoPublish = 0;
-        $iframe->write();
-
-        $this->assertEquals(0, $iframe->AutoPublish);
-
-        $url = WebHookController::getWebookURL();
-        $headers = [
-            'Content-Type' => "application/json"
-        ];
-        $session = null;
-        $data = [
-            'id' => $record['DatawrapperId'],
-            'publicVersion' => 1
-        ];
-        $cookies = null;
-        $body = json_encode($data, JSON_UNESCAPED_SLASHES|JSON_PRETTY_PRINT);
-
-        $request = $this->post($url, $data, $headers, $session, $body, $cookies);
-        $response = json_decode($request->getBody());
-
-        $this->assertEquals(
-            0,
-            $response->count // autopublish turned off
-        );
-
-        // should still be a 200 OK
-        $this->assertEquals(
-            200,
-            $request->getStatusCode(),
-            'Expected success got: ' . $request->getStatusCode() . "/" . $request->getStatusDescription()
-        );
-
-    }
-
-    public function testBadWebhook() {
-
-        $headers = [
-            'Content-Type' => "application/json"
-        ];
-        $session = null;
-        $data = [
-            'id' => 'fake1',
-            'publicVersion' => 1
-        ];
-        $cookies = null;
-        $body = json_encode($data, JSON_UNESCAPED_SLASHES|JSON_PRETTY_PRINT);
-
-        // put together a request with a fake code
-        $path = "/_datawrapperwebhook/submit/notarealcode/";
-        $url = Director::absoluteURL($path);
-
-        $request = $this->post($url, $data, $headers, $session, $body, $cookies);
-        $response = json_decode($request->getBody());
-
-        // should not be a 200 OK
-        $this->assertNotEquals(
-            200,
-            $request->getStatusCode(),
-            'Expected failure got: ' . $request->getStatusCode() . "/" . $request->getStatusDescription()
-        );
-
-        // a valid URL
-        $url = WebHookController::getWebookURL();
-
-
-        $request = $this->get($url);
-        $response = json_decode($request->getBody());
-
-        // GET should not be a 200 OK
-        $this->assertNotEquals(
-            200,
-            $request->getStatusCode(),
-            'Expected failure got: ' . $request->getStatusCode() . "/" . $request->getStatusDescription()
-        );
-
-        // turn off webhooks
-        Config::inst()->set( WebHookController::class, 'webhooks_enabled', false );
-
-        $request = $this->post($url, $data, $headers, $session, $body, $cookies);
-        $response = json_decode($request->getBody());
-
-        // should not be a 200 OK
-        $this->assertNotEquals(
-            200,
-            $request->getStatusCode(),
-            'Expected failure got: ' . $request->getStatusCode() . "/" . $request->getStatusDescription()
-        );
     }
 
 }
---
+++ tests/DatawrapperWebhookTest.php
@@ -0,0 +1,189 @@
+<?php
+
+namespace  NSWDPC\Elemental\Tests\Datawrapper;
+
+use NSWDPC\Datawrapper\WebHookController;
+use NSWDPC\Elemental\Models\Datawrapper\ElementDatawrapper;
+use SilverStripe\Core\Config\Config;
+use SilverStripe\Control\Director;
+use SilverStripe\Dev\FunctionalTest;
+
+/**
+ * Unit test to verify Datawrapper element handling
+ * @author James
+ */
+class DatawrapperWebhookTest extends FunctionalTest
+{
+
+    /**
+     * @inheritdoc
+     */
+    protected $usesDatabase = true;
+
+    /**
+     * @inheritdoc
+     */
+    protected static $fixture_file = "./DataWrapperTest.yml";
+
+    /**
+     * @inheritdoc
+     */
+    public function setUp() {
+        parent::setUp();
+        Config::inst()->set( WebHookController::class, 'webhooks_random_code', 'randomecodeforurl' );
+        Config::inst()->set( WebHookController::class, 'webhooks_enabled', true );
+    }
+
+    /**
+     * Return test record for test
+     */
+    private function createTestRecord() {
+         return $this->objFromFixture(ElementDatawrapper::class, 'webhookrecord');
+    }
+
+    /**
+     * Test webhook controller POST
+     */
+    public function testWebHook() {
+
+        $width = 300;
+        $height = 200;
+
+        $datawrapperId = 'hook1';
+
+        $record = $this->createTestRecord();
+        $record->DatawrapperId = $datawrapperId;
+        $record->DatawrapperVersion = 2;// at this version
+        $record->Width = $width;
+        $record->Height = $height;
+        $record->InputURL = "https://"
+                                . ElementDatawrapper::config()->get('default_host')
+                                . "/"
+                                . $record->DatawrapperId
+                                . "/"
+                                . $record->DatawrapperVersion
+                                . "/";
+        $record->write();
+
+        // POST request to the webhook controller
+        $url = WebHookController::getWebookURL();
+        $headers = [
+            'Content-Type' => "application/json"
+        ];
+        $session = null;
+        $data = [
+            'id' => $datawrapperId,
+            'publicVersion' => 3 // publishing to this version
+        ];
+        $cookies = null;
+        $body = json_encode($data, JSON_UNESCAPED_SLASHES|JSON_PRETTY_PRINT);
+
+        $request = $this->post($url, $data, $headers, $session, $body, $cookies);
+        $response = json_decode($request->getBody());
+
+        $this->assertEquals(
+            1,
+            $response->count // expect 1 element
+        );
+
+        $this->assertEquals(
+            200,
+            $request->getStatusCode(),
+            'Expected success, got: ' . $request->getStatusCode() . "/" . $request->getStatusDescription()
+        );
+
+
+        // turn off autopublish
+        $record->AutoPublish = 0;
+        $record->write();
+
+        $this->assertEquals(0, $record->AutoPublish);
+
+        $url = WebHookController::getWebookURL();
+        $headers = [
+            'Content-Type' => "application/json"
+        ];
+        $session = null;
+        $data = [
+            'id' => $record->DatawrapperId,
+            'publicVersion' => 1
+        ];
+        $cookies = null;
+        $body = json_encode($data, JSON_UNESCAPED_SLASHES|JSON_PRETTY_PRINT);
+
+        $request = $this->post($url, $data, $headers, $session, $body, $cookies);
+        $response = json_decode($request->getBody());
+
+        $this->assertEquals(
+            0,
+            $response->count // autopublish turned off
+        );
+
+        // should still be a 200 OK
+        $this->assertEquals(
+            200,
+            $request->getStatusCode(),
+            'Expected success got: ' . $request->getStatusCode() . "/" . $request->getStatusDescription()
+        );
+
+    }
+
+    /**
+     * Test bad webhook controller POST
+     */
+    public function testBadWebhook() {
+
+        $headers = [
+            'Content-Type' => "application/json"
+        ];
+        $session = null;
+        $data = [
+            'id' => 'fake1',
+            'publicVersion' => 1
+        ];
+        $cookies = null;
+        $body = json_encode($data, JSON_UNESCAPED_SLASHES|JSON_PRETTY_PRINT);
+
+        // put together a request with a fake code
+        $path = "/_datawrapperwebhook/submit/notarealcode/";
+        $url = Director::absoluteURL($path);
+
+        $request = $this->post($url, $data, $headers, $session, $body, $cookies);
+        $response = json_decode($request->getBody());
+
+        // should not be a 200 OK
+        $this->assertNotEquals(
+            200,
+            $request->getStatusCode(),
+            'Expected failure got: ' . $request->getStatusCode() . "/" . $request->getStatusDescription()
+        );
+
+        // a valid URL
+        $url = WebHookController::getWebookURL();
+
+
+        $request = $this->get($url);
+        $response = json_decode($request->getBody());
+
+        // GET should not be a 200 OK
+        $this->assertNotEquals(
+            200,
+            $request->getStatusCode(),
+            'Expected failure got: ' . $request->getStatusCode() . "/" . $request->getStatusDescription()
+        );
+
+        // turn off webhooks
+        Config::inst()->set( WebHookController::class, 'webhooks_enabled', false );
+
+        $request = $this->post($url, $data, $headers, $session, $body, $cookies);
+        $response = json_decode($request->getBody());
+
+        // should not be a 200 OK
+        $this->assertNotEquals(
+            200,
+            $request->getStatusCode(),
+            'Expected failure got: ' . $request->getStatusCode() . "/" . $request->getStatusDescription()
+        );
+    }
+
+}
